16d928d553513b9a4bfd575813eab3c81c9ff87b
fep <fep@f16vbox.(none)>
2012-01-09 19:36:24 -0300

    Primera versión del ejecutable.

---
+++ fucheck.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) 
+{
+	char *docname;
+
+	if (argc <= 1) {
+		printf("Usage: %s docname\n", argv[0]);
+		return(0);
+	}
+
+	return(1);
+}
