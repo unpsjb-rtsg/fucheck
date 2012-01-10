ff62a861a5283ad26dc6669adc50424a8ca84056
fep <franpaez@gmail.com>
2012-01-10 17:49:58 -0300

    Primer prueba con libxml2

--- fucheck.c
+++ fucheck.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <libxml/xmlreader.h>
+
+void processNode(xmlTextReaderPtr reader) {
+	xmlChar *name;
+	name = xmlTextReaderLocalName(reader);
+	printf("%s\n", name);
+	xmlFree(name);
+}
+
+void streamRtsFile(char *file) 
+{
+	xmlTextReaderPtr reader;
+	int ret;
+
+	reader = xmlNewTextReaderFilename(file);
+
+	if (reader != NULL) {
+		ret = xmlTextReaderRead(reader);
+		while (ret == 1) {
+			processNode(reader);
+			ret = xmlTextReaderRead(reader);
+		}
+		xmlFreeTextReader(reader);
+		if (ret != 0) {
+			printf("%s: failed to parse\n", file);
+		}
+	} else {
+		printf("Unable to open %s\n", file);
+	}
+
+	//xmlTextReaderClose(reader);
+}
 
 int main(int argc, char **argv) 
 {
@@ -10,6 +42,9 @@ int main(int argc, char **argv)
 		printf("Usage: %s docname\n", argv[0]);
 		return(0);
 	}
+
+	docname = argv[1];
+	streamRtsFile(docname);
 
 	return(1);
 }
