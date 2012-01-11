efdf7c478ad68e5fb16597300bcdd90acf8d8dc8
fep <franpaez@gmail.com>
2012-01-10 21:01:15 -0300

    XPath para obtener elemento Set

--- fucheck.c
+++ fucheck.c
@@ -2,11 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include <libxml/xmlreader.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+
+int rtsCount;
+int expectedRtsCount;
 
 void processNode(xmlTextReaderPtr reader) {
 	xmlChar *name;
 	name = xmlTextReaderLocalName(reader);
-	printf("%s\n", name);
+	if (xmlStrcmp(name,  (const xmlChar*)"Set") == 0) { 
+		printf("%s\n",  name);
+	}
 	xmlFree(name);
 }
 
@@ -34,6 +41,47 @@ void streamRtsFile(char *file)
 	//xmlTextReaderClose(reader);
 }
 
+void getSetInfo(xmlDocPtr file) {
+	xmlXPathContextPtr context;
+	xmlXPathObjectPtr result;
+
+	xmlChar *xpath = (xmlChar*) "/Set";
+
+	context = xmlXPathNewContext(file);
+	if (context == NULL) {
+		fprintf(stderr, "Error in xmlXPathNewContext\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Selecciona el elemento Set de la raìz del documento
+	result = xmlXPathEvalExpression(xpath, context);
+	if (result == NULL) {
+		fprintf(stderr, "Error in xmlXPathEvalExpression\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (xmlXPathNodeSetIsEmpty(result->nodesetval)) {
+		xmlXPathFreeObject(result);
+		fprintf(stderr, "No Set tag, invalid file.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", result->nodesetval->nodeNr);
+	xmlXPathFreeObject(result);
+}
+
+xmlDocPtr getDoc(char *filename) {
+	xmlDocPtr doc;
+	doc = xmlParseFile(filename);
+
+	if (doc == NULL) {
+		fprintf(stderr, "No se pudo parsear el documento.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return doc;
+}
+
 int main(int argc, char **argv) 
 {
 	char *docname;
@@ -44,7 +92,8 @@ int main(int argc, char **argv)
 	}
 
 	docname = argv[1];
-	streamRtsFile(docname);
+	getSetInfo(getDoc(docname));
+	//streamRtsFile(docname);
 
 	return(1);
 }
