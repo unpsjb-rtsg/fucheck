bd89f1ea946f28a01fbaa4ed652bcae61fedf5d8
fepz <franpaez@gmail.com>
2012-01-15 20:22:43 -0300

    Script para crear un distribuible en formato tar.gz

--- createTarGz.sh
+++ createTarGz.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-tar -cvzf fucheck.tar.gz src/ man/ configure configure.ac install-sh Makefile.in Makefile.am config.h.in \
-	missing depcomp README INSTALL COPYING AUTHORS ChangeLog MANUAL NEWS rts.xml 
+# warning, really fucked up code...
+
+mkdir -p dist/fucheck/src
+mkdir -p dist/fucheck/man
+
+cp src/*.c src/Makefile*  dist/fucheck/src
+cp man/fucheck.1 man/Makefile*  dist/fucheck/man
+cp configure configure.ac install-sh Makefile.in Makefile.am config.h.in missing depcomp \
+	README INSTALL COPYING AUTHORS MANUAL NEWS ChangeLog rts.xml dist/fucheck \
+
+tar -czf fucheck.tar.gz -C dist fucheck --remove-files --exclude-backups --exclude-vcs
+
+rmdir dist/
+
+echo "Done!"
